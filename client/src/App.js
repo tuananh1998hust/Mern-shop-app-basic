3318cfbb45b5a4a25c89da6595a6a15034d2e734
tuananh1998hust <tuananh1998.hust@gmail.com>
2019-05-01 17:25:14 +0700

    Set up React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Container } from "reactstrap";
 import { Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
 // Components
 import AppNavbar from "./components/AppNavbar";
@@ -10,6 +16,11 @@ function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <Container>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/products" component={Products} />
+        <Route path="/cart" component={Cart} />
+      </Container>
     </div>
   );
 }
